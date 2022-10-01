6827dd854e81a4fae30e20cafa430f1c77e14c1c
Noela-Murugi <murugin254@gmail.com>
2022-10-01 18:53:23 +0300

    add updated app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import BotsPage from "./BotsPage";
+ import React from "react";
+ import BotsPage from "./BotsPage";
 
-function App() {
-  return (
-    <div className="App">
-      <BotsPage />
-    </div>
-  );
+ function App() {
+   return (
+     <div className="App">
+       <BotsPage />
+     </div>
+   );
 }
 
-export default App;
+ export default App;
