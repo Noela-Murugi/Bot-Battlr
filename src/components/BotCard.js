23bae5b46114337834c09568e3d2a847a984a339
Noela-Murugi <murugin254@gmail.com>
2022-10-01 20:04:27 +0300

    add card bots, updated BortCard component

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,21 +1,14 @@
 import React from "react";
 
-const botTypeClasses = {
-  Assault: "icon military",
-  Defender: "icon shield",
-  Support: "icon plus circle",
-  Medic: "icon ambulance",
-  Witch: "icon magic",
-  Captain: "icon star",
-};
+function BotCard(props) {
+  let { bot } = props;
 
-function BotCard({ bot }) {
   return (
     <div className="ui column">
       <div
         className="ui card"
         key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        onClick={() => props.includeBot(bot)}
       >
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
@@ -23,7 +16,7 @@ function BotCard({ bot }) {
         <div className="content">
           <div className="header">
             {bot.name}
-            <i className={botTypeClasses[bot.bot_class]} />
+            {/* //             <i className={classBotClasses[bot.bot_class]} /> */}
           </div>
           <div className="meta text-wrap">
             <small>{bot.catchphrase}</small>
@@ -42,18 +35,6 @@ function BotCard({ bot }) {
           <span>
             <i className="icon shield" />
             {bot.armor}
-          </span>
-          <span>
-            <div className="ui center aligned segment basic">
-              <button
-                className="ui mini red button"
-                onClick={() =>
-                  console.log("add code to connect event listener")
-                }
-              >
-                x
-              </button>
-            </div>
           </span>
         </div>
       </div>
