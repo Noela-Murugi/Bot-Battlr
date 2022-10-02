fcaf2b116588bfbfe9c77209917bf2ecddf9c774
Noela-Murugi <murugin254@gmail.com>
2022-10-03 00:14:00 +0300

    include bot collection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,15 +1,24 @@
-import React from "react";
+ import React from "react";
+ import BotCard from "./BotCard";
 
-function BotCollection() {
-  // Your code here
-  return (
-    <div className="ui four column grid">
-      <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
-      </div>
-    </div>
-  );
+
+ function BotCollection(props) {
+   // Your code here
+   function renderBots ()  {
+	return props.bots.map(bot => {
+		return <BotCard key={bot.id} bot={bot} includeBot={props.includeBot}/>
+	})
 }
 
-export default BotCollection;
+   return (
+     <div className="ui four column grid">
+       <div className="row">
+         {/*...and here..*/}
+         Collection of all bots
+		 {renderBots()}
+       </div>
+     </div>
+   );
+ }
+
+ export default BotCollection;
