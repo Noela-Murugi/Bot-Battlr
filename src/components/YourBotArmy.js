fef7c7be85f33f5849c6d768f88ee1884d486f3e
Noela-Murugi <murugin254@gmail.com>
2022-10-02 23:38:02 +0300

    update your bot army section

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,18 +1,26 @@
-import React from "react";
+ import React from "react";
+ import BotCard from "./BotCard";
 
-function YourBotArmy() {
-  //your bot army code here...
+
+ function YourBotArmy(props) {
+   //your bot army code here...
+   function renderBots ()  {
+		return props.bots.map(bot => {
+			return <BotCard key={bot.id} bot={bot} includeBot={props.includeBot}/>
+		})
+  }
 
   return (
-    <div className="ui segment inverted olive bot-army">
-      <div className="ui five column grid">
-        <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
-        </div>
-      </div>
-    </div>
-  );
-}
+     <div className="ui segment inverted olive bot-army">
+       <div className="ui five column grid">
+         <div className="row bot-army-row">
+           {/*...and here...*/}
+           Your Bot Army
+           {renderBots()}
+         </div>
+       </div>
+     </div>
+   );
+ }
 
-export default YourBotArmy;
+ export default YourBotArmy;
